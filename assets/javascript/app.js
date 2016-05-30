8fefc5c58c010f24b008e99b6c8e3671640fa17d
elizaurr <elizaurreyes@yahoo.com>
2016-05-30 19:41:24 -0400

    app.js added and images

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+  console.log("ready");
+
+  var sportOne = {
+    question: 'Who said "Baseball is 90% mental. The other half is physical"?',
+    choice1: 'Yogi Berra',
+    choice2: 'David Ortiz',
+    choice3: 'Derek Jeter',
+    Choice4: 'Babe Ruth',
+    answer: 'Yogi Berra',
+    info: "<img src='assets/images/yogi.jpg'>"
+  };
+
+  var sportTwo = {
+    question: 'Who is the has the most championship in the NBA?',
+    choice1: 'Kobe Bryant',
+    choice2: 'Karl Malone',
+    choice3: 'Patrick Ewing',
+    Choice4: 'Bill Russell',
+    answer: 'Bill Russell',
+    info: "<img src='assets/images/russell.gif'>"
+  };
+
+  var sportThree = {
+    question: 'Who holds the record for the most goals in the NHL playoffs in one season?',
+    choice1: 'Alex Ovechkin',
+    choice2: 'Sidney Crobsy',
+    choice3: 'Jaromir Jagr',
+    Choice4: 'Wayne Gretzky',
+    answer: 'Wayne Gretzky',
+    info: "<img src='assets/images/wayne.jpeg'>"
+  };
+
+  var sportFour = {
+    question: 
+  };
+
+
+}
